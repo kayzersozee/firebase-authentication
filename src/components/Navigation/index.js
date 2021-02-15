ebb4d453edf19b903de6a154bf1957a49877cffa
kayzersozee <63726574+kayzersozee@users.noreply.github.com>
2021-02-15 09:07:45 +0100

    added routing

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,7 +1,26 @@
-const Navigation = () => (
-    <div>
-        <h1>Navigation</h1>
-    </div>
-);
+import React from "react";
+import { Link } from "react-router-dom";
+import * as ROUTES from "../Constants/routes";
 
+const Navigation = () => (
+  <div>
+    <ul>
+      <li>
+        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.LANDING}>Landing</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.HOME}>Home</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.ACCOUNT}>Account</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.ADMIN}>Admin</Link>
+      </li>
+    </ul>
+  </div>
+);
 export default Navigation;
