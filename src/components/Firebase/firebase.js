f64d340c9e02bc2e8f12c67885f898efd7d6cef2
kayzersozee <63726574+kayzersozee@users.noreply.github.com>
2021-02-17 15:07:10 +0100

    added realtime db users, and Admin

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,37 +1,44 @@
 import app from "firebase/app";
 import "firebase/auth";
+import "firebase/database";
 
 const config = {
-  
-    apiKey: "AIzaSyC7nJJCgpxtxRfoHtbT8c--Sn469Y4KHuM",
-    authDomain: "react-firebase-2806f.firebaseapp.com",
-    projectId: "react-firebase-2806f",
-    storageBucket: "react-firebase-2806f.appspot.com",
-    messagingSenderId: "583097000033",
-    appId: "1:583097000033:web:6fbe0e29c78014db700a20",
-    measurementId: "G-LJQRQ36LTK"
-  };
+  apiKey: "AIzaSyC7nJJCgpxtxRfoHtbT8c--Sn469Y4KHuM",
+  authDomain: "react-firebase-2806f.firebaseapp.com",
+  projectId: "react-firebase-2806f",
+  databaseURL:
+    "https://react-firebase-2806f-default-rtdb.europe-west1.firebasedatabase.app",
+  storageBucket: "react-firebase-2806f.appspot.com",
+  messagingSenderId: "583097000033",
+  appId: "1:583097000033:web:6fbe0e29c78014db700a20",
+  measurementId: "G-LJQRQ36LTK",
+};
 
-  class Firebase {
-    constructor() {
-      app.initializeApp(config);
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
 
-      this.auth = app.auth();
+    this.auth = app.auth();
+    this.db = app.database();
 
-      // *** Auth API ***
-    }
-    doCreateUserWithEmailAndPassword = (email, password) =>
-        this.auth.createUserWithEmailAndPassword(email, password);
+    // *** Auth API ***
+  }
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
 
-    doSignInWithEmailAndPassword = (email, password) =>
-        this.auth.signInWithEmailAndPassword(email, password);
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
 
-    doSignOut = () => this.auth.signOut();
+  doSignOut = () => this.auth.signOut();
 
-    doPasswordReset = (email) => this.auth.sendPasswordResetEmail(email);
+  doPasswordReset = (email) => this.auth.sendPasswordResetEmail(email);
 
-    doPasswordUpdate = (password) =>
-          this.auth.currentUser.updatePassword(password);
-    }
-  
-  export default Firebase;
+  doPasswordUpdate = (password) =>
+    this.auth.currentUser.updatePassword(password);
+
+  // *** User API ***
+  user = (uid) => this.db.ref(`users/${uid}`);
+  users = () => this.db.ref("users");
+}
+
+export default Firebase;
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -24,7 +24,9 @@ const NavigationAuth = () => (
       <li>
         <Link to={ROUTES.ACCOUNT}>Account</Link>
       </li>
-
+      <li>
+        <Link to={ROUTES.ADMIN}>Admin</Link>
+      </li>
       <li>
         <SignOutButton />
       </li>
