d7422fb4f055db7a9bc12885bc5ed12ff21a3b8f
kayzersozee <63726574+kayzersozee@users.noreply.github.com>
2021-02-22 10:09:04 +0100

    added roles

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import { Link, withRouter } from "react-router-dom";
-import * as ROUTES from "../Constants/routes";
 import { withFirebase } from "../Firebase";
 import { compose } from "recompose";
+
+import * as ROLES from "../Constants/roles";
+import * as ROUTES from "../Constants/routes";
 
 const SignUpPage = () => (
   <div>
@@ -16,6 +18,7 @@ const INITIAL_STATE = {
   email: "",
   passwordOne: "",
   passwordTwo: "",
+  isAdmin: false,
   error: null,
 };
 
@@ -26,7 +29,12 @@ class SignUpFormBase extends Component {
   }
 
   onSubmit = (event) => {
-    const { username, email, passwordOne } = this.state;
+    const { username, email, passwordOne, isAdmin } = this.state;
+
+    const roles = {};
+    if (isAdmin) {
+      roles[ROLES.ADMIN] = ROLES.ADMIN;
+    }
 
     this.props.firebase
       .doCreateUserWithEmailAndPassword(email, passwordOne)
@@ -36,6 +44,7 @@ class SignUpFormBase extends Component {
         return this.props.firebase.user(authUser.user.uid).set({
           username,
           email,
+          roles,
         });
       })
 
@@ -48,11 +57,24 @@ class SignUpFormBase extends Component {
       });
     event.preventDefault();
   };
+
   onChange = (event) => {
     this.setState({ [event.target.name]: event.target.value });
   };
+
+  onChangeCheckbox = (event) => {
+    this.setState({ [event.target.name]: event.target.checked });
+  };
+
   render() {
-    const { username, email, passwordOne, passwordTwo, error } = this.state;
+    const {
+      username,
+      email,
+      passwordOne,
+      passwordTwo,
+      isAdmin,
+      error,
+    } = this.state;
 
     const isInvalid =
       passwordOne !== passwordTwo ||
@@ -89,6 +111,17 @@ class SignUpFormBase extends Component {
           type="password"
           placeholder="Confirm Password"
         />
+
+        <label>
+          Admin:
+          <input
+            name="isAdmin"
+            type="checkbox"
+            checked={isAdmin}
+            onChange={this.onChangeCheckbox}
+          />
+        </label>
+
         <button disabled={isInvalid} type="submit">
           Sign Up
         </button>
