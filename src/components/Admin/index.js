700f2ba50a51463912755bf2bb32aa906ea87568
kayzersozee <63726574+kayzersozee@users.noreply.github.com>
2021-02-22 11:22:25 +0100

    added session persistence

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
+import { compose } from "recompose";
 import { withFirebase } from "../Firebase";
+import { withAuthorization } from "../Session";
+import * as ROLES from "../Constants/roles";
 
 class AdminPage extends Component {
   constructor(props) {
@@ -32,6 +35,7 @@ class AdminPage extends Component {
     return (
       <div>
         <h1>Admin</h1>
+        <p>The Admin Page is accessible by every signed in admin user.</p>
         {loading && <div>Loading ...</div>}
         <UserList users={users} />
       </div>
@@ -55,4 +59,9 @@ const UserList = ({ users }) => (
     ))}
   </ul>
 );
-export default withFirebase(AdminPage);
+
+const condition = (authUser) => authUser && !!authUser.roles[ROLES.ADMIN];
+
+export default compose(withAuthorization(condition), withFirebase)(AdminPage);
+
+/* export default withFirebase(AdminPage); */
